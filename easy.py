871c4df549e92d153f38a00d62307b37b427ca0a
Tommy Ju <tommyju25@gmail.com>
2023-10-15 15:12:58 -0700

    Solve problem in easy.py

--- easy.py
+++ easy.py
@@ -14,3 +14,38 @@ You must provide a full docstring for this function including all pre- and post-
 You must create two useful and different doctests for this function.
 No main function is required.
 """
+
+"""
+Tommy Ju
+A01347715
+Set F
+"""
+
+
+def count_evens(readings):
+    """
+    Count and add the number of even integers in a list
+
+    :param readings: a list of integers
+    :precondition: readings must be non-empty
+    :postcondition: correctly identifies the even integers and their sum
+    :return: a tuple representing the number of even integers, and the sum
+
+    >>> count_evens([1, 2, 3, 4])
+    (2, 6)
+    >>> count_evens([0, 1, -1, 2])
+    (2, 2)
+    >>> count_evens([-2, 2, -2, 2])
+    (4, 0)
+    """
+
+    even_integers = []
+    for i in readings:
+        # integer is even
+        if i % 2 == 0:
+            even_integers.append(i)
+        # integer is odd
+        else:
+            continue
+
+    return len(even_integers), sum(even_integers)
