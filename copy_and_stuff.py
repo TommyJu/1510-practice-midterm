58a15138e0d4b46c80399983eaf9b0bfe5e09509
Tommy Ju <tommyju25@gmail.com>
2023-10-26 12:51:38 -0700

    Ready for midtermsgit add .

--- copy_and_stuff.py
+++ copy_and_stuff.py
@@ -4,16 +4,19 @@ A01347715
 """
 import copy
 
-some_list = [1, 2, 3]
+some_list = [[1, 2, 3], 4, 5]
+alias = some_list
 shallow_copy = copy.copy(some_list)
 deep_copy = copy.deepcopy(some_list)
 
-print (id(some_list))
-print (id(shallow_copy))
-print (id(deep_copy))
+print ("list:", id(some_list))
+print ("alias:", id(alias))
+print ("shallow copy:", id(shallow_copy))
+print ("deep copy:", id(deep_copy))
 
-print ('-' * 5)
+print ('-' * 10)
 
-print (id(some_list[0]))
-print (id(shallow_copy[0]))
-print (id(deep_copy[0]))
+print ("list:", id(some_list[0]))
+print ("alias:", id(alias[0]))
+print ("shallow copy:", id(shallow_copy[0]))
+print ("deep copy:", id(deep_copy[0]))
